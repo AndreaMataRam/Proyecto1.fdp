c425d29a48d232a3c2733710d85bc81a3e82a389
Andrea Mata Ram <andrea.mata@ingenieria.unam.edu>
2021-10-04 18:56:45 -0500

    Initial Commit

---
+++ Proyecto1.fdp/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Proyecto1.fdp
+//
+//  Created by Andrea Mata Ramírez on 04/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
