2fd72fa5644fffec2536a789b738baaf9d6aeb07
Andrea Mata Ram <andrea.mata@ingenieria.unam.edu>
2021-10-04 19:23:20 -0500

    cdv

--- Proyecto1.fdp/cdb.c
+++ Proyecto1.fdp/cdb.c
@@ -2,32 +2,27 @@
 //  cdb.c
 //  Proyecto1.fdp
 //
+//  Created by Andrea Mata Ramírez on 04/10/21.
+//
 
-#include <stdio.h>
-#include <string.h>
+#include "cdb.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
 
 int main(){
-    int num, t;
+    int num = 0, pot = 0;
     char binario[100];
 
-    // Lectura del número a convertir
-    scanf("%d", &num);
+    scanf("%s", &binario);
 
-    //Copiando  la cadena vacía a la variable binario
-    strcpy(binario, "");
-
-    do{
-        t= num % 2;
-        num = (int)(num /2);
-        if(t==1) strcat(binario, "1");
-        else strcat(binario, "0");
-    }while(num!=1 && num!=0);
-                                //"1\0"
-    if(num== 1) strcat(binario, "1");
-
-    for(int i =strlen(binario)-1; i >=0; i-- ){
-        printf("%c", binario[i]);
+    for(int i = strlen(binario)-1; i>=0; i--){
+        if(binario[i]=='1')
+            num= num + pow(2, pot);
+        pot = pot + 1;
     }
-    printf("\n");
+
+    printf("%d\n", num);
     return 0;
 }
